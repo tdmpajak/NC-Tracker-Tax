14fff72bffe945a3b6a56bc45b2f10e1c7c7cf38
tdmpajak <tdm.pajak10@gmail.com>
2026-07-22 12:08:09 +0700

    Add 'DMX' to branch dropdown list

--- assets/branch-config.js
+++ assets/branch-config.js
@@ -20,7 +20,7 @@
 // Urutan cabang yang muncul di dropdown form kirim berkas
 const BRANCH_LIST = [
   'DLR','DLL','DLY','DLP','DMP','DLA','DLQ','DLO','DMR','DLF','DLH','DLB','DLV','DLE','DLJ','DMN',
-  'DLS','DLX','DLZ','DLI','DMM','DLK','DLU','DLG','DLW','DLN','DLM','DLT','DLD','DMK','MML','MMM','MMT','HHO'
+  'DLS','DLX','DLZ','DLI','DMM','DLK','DLU','DLG','DLW','DLN','DLM','DLT','DLD','DMK','MML','MMM','MMT','HHO','DMX'
 ];
 
 // Password otorisasi admin saat menyimpan hasil verifikasi di tracking.html
